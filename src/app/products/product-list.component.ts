c78b30fff709fc9f126467cb10d3c58d8322a8a2
jokeeras <jokeeras@yahoo.com>
2019-10-23 17:45:59 +0300

    up

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -25,32 +26,13 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2019",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-      this.filteredProducts = this.products;
-      this.listFilter = '';
+    constructor(private productService: ProductService) {
+    }
+
+    onRatingClicked(message: string): void{
+      this.pageTitle = 'Product List: ' + message;
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -64,6 +46,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      console.log('OnInit');
+      this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+
     }
 }
